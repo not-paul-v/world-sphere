08ad07fb0b4ad43b2fd6060ff906b3c434dd75e3
itspauv <p.viehmann01@gmail.com>
2022-09-10 20:33:12 +0200

    fix: added packages to transpile params

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,4 @@
-const withTM = require("next-transpile-modules")(["ui"]);
+const withTM = require("next-transpile-modules")(["@world-sphere/react", "@world-sphere/core"]);
 
 module.exports = withTM({
   reactStrictMode: true,
