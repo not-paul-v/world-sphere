27ff462c008296ffa7c2353ea013f0e6d14566aa
itspauv <p.viehmann01@gmail.com>
2022-09-20 19:38:40 +0200

    feat: added test cases for countryHelper utility

---
+++ packages/world-sphere-data/__test__/countryHelper.test.ts
@@ -0,0 +1,16 @@
+import { CountryHelper } from "../utils/countryHelper";
+
+const helper = new CountryHelper();
+
+it("should return correct country", () => {
+    const country = helper.getCountryFromLatLon(52.5, 13.5);
+
+    expect(country).not.toBeNull();
+    expect(country).toBe("Germany");
+});
+
+it("should not return a country", () => {
+    const country = helper.getCountryFromLatLon(58.5, -16);
+
+    expect(country).toBeNull();
+});
