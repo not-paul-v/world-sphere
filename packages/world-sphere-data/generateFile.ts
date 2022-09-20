b52f8d247cf26bcdc937a3c80d7db2de2c7c43e1
itspauv <p.viehmann01@gmail.com>
2022-09-20 19:37:48 +0200

    refactor: cleaned up getCoordinates

--- packages/world-sphere-data/generateFile.ts
+++ packages/world-sphere-data/generateFile.ts
@@ -2,12 +2,6 @@ import { Coordinate } from "@world-sphere/core/types/types";
 import { CountryHelper } from "./utils/countryHelper";
 import * as fs from "fs";
 
-const DEG2RAD = Math.PI / 180;
-
-const GLOBE_RADIUS = 1;
-const ROWS = 350;
-const DENSITY = 120;
-
 export function generateFile(filename: string) {
     const coordinates = getCoordinates();
     fs.writeFileSync(filename, JSON.stringify(coordinates));
@@ -20,6 +14,23 @@ export function getCoordinates() {
         [key: string]: Coordinate[];
     } = {};
 
+    forEachLatLon((lat, lon) => {
+        const country = countryHelper.getCountryFromLatLon(lat, lon);
+        if (country) {
+            if (country in coordinates) coordinates[country].push([lat, lon]);
+            else coordinates[country] = [[lat, lon]];
+        }
+    });
+
+    return coordinates;
+}
+
+function forEachLatLon(callbackFn: (lat: number, lon: number) => void) {
+    const DEG2RAD = Math.PI / 180;
+    const GLOBE_RADIUS = 1;
+    const ROWS = 350;
+    const DENSITY = 120;
+
     for (let lat = -90; lat <= 85; lat += 180 / ROWS) {
         const radius = Math.cos(Math.abs(lat) * DEG2RAD) * GLOBE_RADIUS;
         const circumference = radius * Math.PI * 2;
@@ -27,14 +38,7 @@ export function getCoordinates() {
         for (let x = 0; x < dotsForLat; x++) {
             const lon = -180 + (x * 360) / dotsForLat;
 
-            const country = countryHelper.getCountryFromLatLon(lat, lon);
-            if (country) {
-                if (coordinates[country] === undefined)
-                    coordinates[country] = [[lat, lon]];
-                else coordinates[country].push([lat, lon]);
-            }
+            callbackFn(lat, lon);
         }
     }
-
-    return coordinates;
 }
