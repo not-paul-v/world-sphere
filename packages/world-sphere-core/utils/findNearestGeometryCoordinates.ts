a91888f4fc41bf7bca02db4c0ebff965f2042b20
not-paul-v <p.viehmann01@gmail.com>
2022-11-11 21:07:24 +0100

    feat: find nearest geometry coordinate to given coordinate

---
+++ packages/world-sphere-core/utils/findNearestGeometryCoordinates.ts
@@ -0,0 +1,26 @@
+import data from "../assets/countriesData.json";
+import { Coordinate } from "../types/types";
+
+export const findNearestGeometryCoordinates = (lat: number, lon: number): Coordinate | undefined => {
+    const roundedLat = Math.round(lat);
+    const roundedLon = Math.round(lon);
+    
+    let key: keyof typeof data;
+    for (key in data) {
+        const coordinates = data[key];
+        const minLat = coordinates[0][0];
+        const maxLat = coordinates[coordinates.length-1][0];
+
+        if (roundedLat > maxLat || roundedLat < minLat) continue
+
+        for (let coordinate of coordinates) {
+            const isCorrectLat = roundedLat === Math.round(coordinate[0])
+            if (!isCorrectLat) continue;
+            const isCorrectLon = roundedLon === Math.round(coordinate[1])
+            if (!isCorrectLon) continue;
+
+            return coordinate as Coordinate;
+        }
+    }
+    return undefined
+}
