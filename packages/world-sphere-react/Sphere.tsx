a89db4eb2e406f9bbff7bd8cdcf54e8b6eafac17
not-paul-v <p.viehmann01@gmail.com>
2022-11-11 18:44:05 +0100

    refactor: removed more debug code

--- packages/world-sphere-react/Sphere.tsx
+++ packages/world-sphere-react/Sphere.tsx
@@ -1,54 +1,15 @@
 import { useState, useRef, useEffect } from "react";
-import { useFrame } from "@react-three/fiber";
 import {
     BufferGeometry,
     Group,
-    Raycaster,
-    Vector2,
     Mesh,
-    Vector3,
-    LineBasicMaterial,
-    Line,
 } from "three";
 import { MemoizedSphereCountry } from "./SphereCountry";
-import { loadMergedGeometries, HoverHelper } from "@world-sphere/core";
+import { loadMergedGeometries } from "@world-sphere/core";
 import { acceleratedRaycast } from "three-mesh-bvh";
 import { useCountryHovered } from "./hooks/useCountryHovered";
-import { LineGeometry } from "three/examples/jsm/lines/LineGeometry";
 
 Mesh.prototype.raycast = acceleratedRaycast;
-
-const getLine = (startPoint: Vector3, direction: Vector3) => {
-    direction.normalize();
-
-    // 0.8 0.21 0.55
-    var distance = 100; // at what distance to determine pointB
-
-    const endPoint = new Vector3();
-    endPoint.addVectors (startPoint, direction.multiplyScalar( distance ) );
-    // console.log(startPoint.x, startPoint.y, startPoint.z);
-    
-    const geometry = new BufferGeometry().setFromPoints([startPoint, endPoint]);
-    return geometry
-}
-
-
-function LineI ({start, direction}: {start: Vector3, direction: Vector3}) {
-    const ref = useRef<Line>(null);
-
-    useFrame(() => {
-        if(ref.current){
-            ref.current.geometry = getLine(start, direction);
-        }
-    })
-    return (
-        // @ts-ignore
-        <line ref={ref}>
-            <bufferGeometry />
-            <lineBasicMaterial color="hotpink" linewidth={100}/>
-        </line>
-    )
-}
 
 export function Sphere() {
     const [countryGeometries, setCountryGeometries] = useState<
