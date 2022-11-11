18a12374933c1be2ab613362aead31214511fb3c
not-paul-v <p.viehmann01@gmail.com>
2022-11-11 18:43:06 +0100

    refactor: remove debug code

--- packages/world-sphere-react/Sphere.tsx
+++ packages/world-sphere-react/Sphere.tsx
@@ -54,71 +54,11 @@ export function Sphere() {
     const [countryGeometries, setCountryGeometries] = useState<
         { key: string; geometry: BufferGeometry }[] | undefined
     >();
-    const [lines, setLines] = useState<{start: Vector3, direction: Vector3}[]>([{
-        start: new Vector3(0.8, 0.21, 0.55),
-        direction: new Vector3(0, 0, -1)
-    }, {
-        start: new Vector3(0, 0, 0),
-        direction: new Vector3(0, 0, -1)
-    }, {
-        start: new Vector3(0, 0, 0),
-        direction: new Vector3(0, 0, -1)
-    }, {
-        start: new Vector3(0, 0, 0),
-        direction: new Vector3(0, 0, -1)
-    }]);
-    const [pointOnSphere, setPointOnSphere] = useState<Vector3 | undefined>();
 
     const countriesRef = useRef<Group>(null);
     const sphereRef = useRef<Group>(null);
 
     const hoveredCountry = useCountryHovered(countriesRef.current, sphereRef.current);
-
-    useFrame(({mouse, camera}) => {
-        const ray = new Raycaster();
-        const mousePointer = new Vector2(mouse.x, mouse.y);
-
-        ray.setFromCamera(mousePointer, camera);
-        ray.firstHitOnly = true;
-
-        if (!sphereRef.current) return;
-
-        const intersections = ray.intersectObject(sphereRef.current);
-        const intersectionPoint = intersections[0]?.point;
-
-        if (!intersectionPoint) return;
-        setPointOnSphere(intersectionPoint);
-        
-        const origin = new Vector3(0, 0, 0);
-
-        const dir = new Vector3(0, 0, 0);
-        dir.subVectors(origin, intersectionPoint).normalize();
-
-        const leftRayVector = new Vector3();
-        const rightRayVector = new Vector3();
-        const topRayVector = new Vector3();
-        const bottomRayVector = new Vector3();
-
-        rightRayVector.crossVectors(dir, new Vector3(0, 1, 0))
-        leftRayVector.crossVectors(dir, new Vector3(0, -1, 0));
-        
-        topRayVector.crossVectors(dir, rightRayVector);
-        bottomRayVector.crossVectors(dir, leftRayVector);
-
-        lines[0].direction = leftRayVector.normalize();
-        lines[0].start = intersectionPoint;
-
-        lines[1].direction = rightRayVector.normalize();
-        lines[1].start = intersectionPoint;
-
-        lines[2].direction = topRayVector.normalize();
-        lines[2].start = intersectionPoint;
-
-        lines[3].direction = bottomRayVector.normalize();
-        lines[3].start = intersectionPoint;
-
-        // lines[0].direction = dir.clone();
-    });
 
     useEffect(() => {
         loadMergedGeometries().then((data) => setCountryGeometries(data));
