54e62289ac121696bb609651eee78cc7a4ddae6b
not-paul-v <p.viehmann01@gmail.com>
2022-11-14 20:38:58 +0100

    refactor: remove debug export

--- packages/world-sphere-core/index.ts
+++ packages/world-sphere-core/index.ts
@@ -1,3 +1,2 @@
 export { loadGeometries, loadMergedGeometries } from "./loadGeometries";
 export { HoverHelper } from "./HoverHelper";
-export { findNearestGeometryCoordinates } from "./utils/findNearestGeometryCoordinates";
