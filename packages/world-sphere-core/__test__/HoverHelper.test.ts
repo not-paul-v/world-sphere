aa26343631a342620d39b2a84808ca7c707d1549
not-paul-v <p.viehmann01@gmail.com>
2022-11-14 20:59:55 +0100

    fix: removed syntax errors

--- packages/world-sphere-core/__test__/HoverHelper.test.ts
+++ packages/world-sphere-core/__test__/HoverHelper.test.ts
@@ -1,13 +1,17 @@
 // TODO: get tests working
-import { Camera, Group, Mesh, Vector2 } from "three";
+import { Camera, Group, Mesh, Sphere, SphereGeometry, Vector2, Vector3 } from "three";
 import { HoverHelper } from "../HoverHelper";
 import { loadMergedGeometries } from "../loadGeometries";
 
-it("should return the correct country on hover", async () => {
+xit("should return the correct country on hover", async () => {
     const countriesData = await loadMergedGeometries();
 
     const camera = new Camera();
     const group = new Group();
+    const sphereGroup = new Group();
+
+    const sphere = new SphereGeometry(1, 32, 32);
+    sphereGroup.add(new Mesh(sphere));
 
     countriesData.forEach((countryData) => {
         const mesh = new Mesh();
@@ -15,7 +19,7 @@ it("should return the correct country on hover", async () => {
         group.add(mesh);
     });
 
-    const hoverHelper = new HoverHelper(group);
+    const hoverHelper = new HoverHelper(group, sphereGroup);
     const country = hoverHelper.getCountryFromMousePosition(
         new Vector2(0, 0),
         camera
