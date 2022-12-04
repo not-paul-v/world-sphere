fff6bf54fbae2bffbde302c12cca56d1afd30db0
itspauv <p.viehmann01@gmail.com>
2022-12-04 21:25:24 +0100

    feat: added jsdoc to explain config parameter

--- packages/world-sphere-core/loadGeometries.ts
+++ packages/world-sphere-core/loadGeometries.ts
@@ -1,13 +1,24 @@
 import { BufferGeometry } from "three";
-import { CountryData, MergedCountryData } from "@world-sphere/types";
+import {
+    CountryData,
+    MergedCountryData,
+    GenerationConfig,
+} from "@world-sphere/types";
 import { GeometryHelper } from "./utils/GeometryHelper";
 import { mergeBufferGeometries } from "three/examples/jsm/utils/BufferGeometryUtils";
 import { getCoordinates } from "@world-sphere/data";
 
-export async function loadMergedGeometries(): Promise<MergedCountryData> {
+/**
+ *
+ * @param config [(optional) You can pass the globe's radius, the number of rows and the density of those rows. If you don't pass any config the default coordinate data will be loaded from a pregenerated file. This will be a lot faster than if you provide custom values.
+ * @returns A list of merged geometries for the specific countries.
+ */
+export async function loadMergedGeometries(
+    config?: GenerationConfig
+): Promise<MergedCountryData> {
     const mergedCountryData: MergedCountryData = [];
 
-    const geos = await loadGeometries();
+    const geos = await loadGeometries(config);
 
     for (let countryData of geos) {
         if (countryData.geometries.length > 0) {
@@ -25,11 +36,13 @@ export async function loadMergedGeometries(): Promise<MergedCountryData> {
  *
  * @returns A list of objects of type CountryData; Containing the name of a country and the BufferGeometries to draw that country
  */
-export async function loadGeometries(): Promise<CountryData[]> {
+export async function loadGeometries(
+    config?: GenerationConfig
+): Promise<CountryData[]> {
     const geometryHelper = new GeometryHelper();
     const countriesData: CountryData[] = [];
 
-    const data = await getCoordinates();
+    const data = await getCoordinates(config);
 
     let key: keyof typeof data;
     for (key in data) {
