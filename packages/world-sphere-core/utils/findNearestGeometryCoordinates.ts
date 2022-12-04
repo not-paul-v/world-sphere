9433f0c85952bf1c8dd4d9ca8658117198481a56
itspauv <p.viehmann01@gmail.com>
2022-12-04 21:17:25 +0100

    refactor: moved types to own package

--- packages/world-sphere-core/utils/findNearestGeometryCoordinates.ts
+++ packages/world-sphere-core/utils/findNearestGeometryCoordinates.ts
@@ -1,26 +1,29 @@
 import data from "../assets/countriesData.json";
-import { Coordinate } from "../types/types";
+import { Coordinate } from "@world-sphere/types";
 
-export const findNearestGeometryCoordinates = (lat: number, lon: number): Coordinate => {
+export const findNearestGeometryCoordinates = (
+    lat: number,
+    lon: number
+): Coordinate => {
     const roundedLat = Math.round(lat);
     const roundedLon = Math.round(lon);
-    
+
     let key: keyof typeof data;
     for (key in data) {
         const coordinates = data[key];
         const minLat = coordinates[0][0];
-        const maxLat = coordinates[coordinates.length-1][0];
+        const maxLat = coordinates[coordinates.length - 1][0];
 
-        if (roundedLat > maxLat || roundedLat < minLat) continue
+        if (roundedLat > maxLat || roundedLat < minLat) continue;
 
         for (let coordinate of coordinates) {
-            const isCorrectLat = roundedLat === Math.round(coordinate[0])
+            const isCorrectLat = roundedLat === Math.round(coordinate[0]);
             if (!isCorrectLat) continue;
-            const isCorrectLon = roundedLon === Math.round(coordinate[1])
+            const isCorrectLon = roundedLon === Math.round(coordinate[1]);
             if (!isCorrectLon) continue;
 
             return coordinate as Coordinate;
         }
     }
     return [lat, lon] as Coordinate;
-}
+};
