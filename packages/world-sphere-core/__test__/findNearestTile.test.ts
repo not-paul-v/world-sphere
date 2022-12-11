c1e0e955c0b0b284c2f6b2a1baa274cefa7e98b3
not-paul-v <p.viehmann01@gmail.com>
2022-12-11 12:31:04 +0100

    feat: @world-sphere/core tests

--- packages/world-sphere-core/__test__/findNearestTile.test.ts
+++ packages/world-sphere-core/__test__/findNearestTile.test.ts
@@ -1,11 +1,30 @@
 import { findNearestGeometryCoordinates } from "../utils/findNearestGeometryCoordinates";
 
-it("should find nearest tile for given coordinates on land", () => {
-    const nearestTileCoordinates = findNearestGeometryCoordinates(52.520008, 13.404954);
-    
-    expect(nearestTileCoordinates.length).toBe(2);
-    expect(nearestTileCoordinates[0]).toBeGreaterThanOrEqual(52);
-    expect(nearestTileCoordinates[0]).toBeLessThanOrEqual(53);
-    expect(nearestTileCoordinates[1]).toBeGreaterThanOrEqual(12);
-    expect(nearestTileCoordinates[1]).toBeLessThanOrEqual(14);
+describe("findNearestTile test", () => {
+    it("should find nearest tile for given coordinates on land", () => {
+        // when
+        const nearestTileCoordinates = findNearestGeometryCoordinates(
+            52.520008,
+            13.404954
+        );
+
+        // then
+        expect(nearestTileCoordinates).toBeDefined();
+
+        if (nearestTileCoordinates) {
+            expect(nearestTileCoordinates.length).toBe(2);
+            expect(nearestTileCoordinates[0]).toBeGreaterThanOrEqual(52);
+            expect(nearestTileCoordinates[0]).toBeLessThanOrEqual(53);
+            expect(nearestTileCoordinates[1]).toBeGreaterThanOrEqual(12);
+            expect(nearestTileCoordinates[1]).toBeLessThanOrEqual(14);
+        }
+    });
+
+    it("should not find nearest tile for given coordinates in the ocean", () => {
+        // when
+        const nearestTileCoordinates = findNearestGeometryCoordinates(-20, -7);
+
+        // then
+        expect(nearestTileCoordinates).not.toBeDefined();
+    });
 });
--- packages/world-sphere-core/__test__/loadGeometries.test.ts
+++ packages/world-sphere-core/__test__/loadGeometries.test.ts
@@ -1,10 +1,25 @@
-import { loadGeometries } from "../loadGeometries";
+import { loadGeometries, loadMergedGeometries } from "../loadGeometries";
 
-it("should load the geometries", async () => {
-    const geometries = await loadGeometries();
+describe("load geometries test", () => {
+    it("should load the geometries", async () => {
+        // when
+        const geometries = await loadGeometries();
 
-    expect(geometries.length).toBe(176);
-    geometries.forEach((data) => {
-        expect(data.geometries.length).toBeGreaterThan(0);
+        // then
+        expect(geometries.length).toBe(176);
+        geometries.forEach((data) => {
+            expect(data.geometries.length).toBeGreaterThan(0);
+        });
+    });
+
+    it("should load the merged geometries", async () => {
+        // when
+        const geometries = await loadMergedGeometries();
+
+        // then
+        expect(geometries.length).toBe(176);
+        geometries.forEach((data) => {
+            expect(data.geometry).toBeDefined();
+        });
     });
 });
